0acb9ffed06074ec6c2c0e6d99e7e0c10d8d67a5
Kartikeya Saxena <44299430+ktk53x@users.noreply.github.com>
2019-10-24 13:26:13 +0530

    Update lab_10_extra_credits.cpp

--- lab_10_extra_credits.cpp
+++ lab_10_extra_credits.cpp
@@ -11,16 +11,16 @@ bool is_visited(int i, int j)
     return visited[i][j];
 }
 
-void reset_matrix(int n, int m)
+void reset_matrix(int m, int n)
 {
-    for(int i = 0; i < n; i++)
-        for(int j = 0; j < m; j++)
-            visited[i][j] = false;
+    for(int i = 0; i < m; i++)
+        for(int j = 0; j < n; j++)
+            visited[i][j] = 0;
 }
 
 bool is_valid(int i, int j, int color, vector<vector<int>>& grid)
 {
-    if(i >= 0 && j >= 0 && i < n && j < m && !is_visited(i, j) && grid[i][j] == color)
+    if(i >= 0 && j >= 0 && i < m && j < n && !is_visited(i, j) && grid[i][j] == color)
         return true;
     else 
         return false;
@@ -38,13 +38,13 @@ void depth_first_search(int i, int j, int color, vector<vector<int>>& grid)
     }
 }
 
-int largest_contiguous_component(int n, int m, vector<vector<int>>& grid)
+int largest_contiguous_component(int m, int n, vector<vector<int>>& grid)
 {
-    reset_matrix(n, m);
+    reset_matrix(m, n);
     max_count = 0;
-    for(int i = 0; i < n; i++)
+    for(int i = 0; i < m; i++)
     {
-        for(int j = 0; j < m; j++)
+        for(int j = 0; j < n; j++)
         {
             count = 0;
             if(!is_visited(i, j))
@@ -61,15 +61,15 @@ int largest_contiguous_component(int n, int m, vector<vector<int>>& grid)
 
 int main()
 {
-    cin >> n >> m >> c;
-    visited.reserve(n);
-    for(int i = 0; i < n; i++)
-        visited[i].reserve(m);
-    vector<vector<int>> grid(n, vector<int>(m));
-    for(int i = 0; i < n; i++)
-        for(int j = 0; j < m; j++)
+    cin >> m >> n >> c;
+    visited.reserve(m);
+    for(int i = 0; i < m; i++)
+        visited[i].reserve(n);
+    vector<vector<int>> grid(m, vector<int>(n));
+    for(int i = 0; i < m; i++)
+        for(int j = 0; j < n; j++)
             cin >> grid[i][j];
     
-    cout << largest_contiguous_component(n, m, grid) << ' ' << max_color << '\n';
+    cout << largest_contiguous_component(m, n, grid) << ' ' << max_color << '\n';
     return 0;
 }
