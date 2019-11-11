d0c02165edb207e0fc8eeb9b55e2b2d9508a2403
Kartikeya Saxena <44299430+ktk53x@users.noreply.github.com>
2019-11-11 16:12:22 +0530

    Add files via upload

---
+++ lab_13.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <set>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    set<string> s;
+    string temp, rtemp;
+    bool flag = false;
+    while(n--)
+    {
+        cin >> temp;
+        rtemp = temp;
+        if(!flag)
+        {
+            reverse(rtemp.begin(), rtemp.end());
+            if(temp.compare(rtemp))
+            {
+                if(s.find(rtemp) != s.end())
+                    flag = 1;
+                s.insert(temp);
+            }
+        }
+    }
+    if(flag)
+        cout << "YES\n";
+    else
+        cout << "NO\n";
+    return 0;
+}
